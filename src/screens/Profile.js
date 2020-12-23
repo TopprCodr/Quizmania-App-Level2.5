0f424cc0e962d4e54441d31d21b392c29b15bde9
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2020-12-23 21:48:23 +0530

    Update Profile.js

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity, Image, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 import BasicButton from "../components/BasicButton";
 
 export default function Profile() {
